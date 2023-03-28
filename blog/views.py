ad0e9c2957b4c5394611fcaa84a7467cafc25536
Ulugbek <ulugxodja@gmail.com>
2023-03-28 20:21:13 +0500

    add LoginRequiredMixin

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -14,17 +15,17 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
